d347a88abf6105d76d6ac348d3dbc80cfd6b99f3
Colin Chen <chencolin.me@gmail.com>
2018-11-12 00:04:46 -0800

    Subscriptions
    Using OnMount hook for Mounting & Unmounting of subscribeToMore for the createAuction Listing and listAuctions.

--- src/Auctions.tsx
+++ src/Auctions.tsx
@@ -4,11 +4,15 @@ import { listAuctions } from './graphql/queries';
 import gql from 'graphql-tag';
 import { ListAuctionsQueryVariables, ListAuctionsQuery } from './API';
 import { AuctionCard } from './AuctionCard';
+import { OnMount } from './components/OnMount';
+import { buildSubscription } from 'aws-appsync';
+import { onCreateAuction } from './graphql/subscriptions';
 
 export const Auctions = () => {
   return (
     <Query<ListAuctionsQuery, ListAuctionsQueryVariables> query={gql(listAuctions)} variables={{ limit: 100 }}>
-      {({ data, loading }) => {
+      {({ data, loading, subscribeToMore }) => {
+        // Subscriptions: In your Query component, there is a subscribeToMore available. We want to call it at the beginning & at the end. For subscribeToMore, we want inside a lifecycle method that mounts & unmounts. We will use a hook for this.
         if (loading || !data || !data.listAuctions || !data.listAuctions.items) {
           return null;
         }
@@ -21,6 +25,13 @@ export const Auctions = () => {
             gridTemplateColumns: 'repeat(4, 1fr)',
             gridGap: 10,
           }}>
+
+          
+          <OnMount onEffect={() => {
+             return subscribeToMore(
+               buildSubscription(gql(onCreateAuction), gql(listAuctions))
+            );
+          }}/>
             {data.listAuctions.items.map(x => <AuctionCard key={x!.id} name={x!.name} price={x!.price}  />)}
           </div>
         )
