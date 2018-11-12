26fc362dc0785d561b6803224679fb46252e6f1c
Colin Chen <chencolin.me@gmail.com>
2018-11-11 21:41:38 -0800

    Auction Card Component & Auctions Component that maps through available listings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { CreateAuctionForm } from './CreateAuctionForm';
+import { Auctions } from './Auctions';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CreateAuctionForm />
+        <Auctions />
       </div>
     );
   }
---
+++ src/Auctions.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Query } from 'react-apollo';
+import { listAuctions } from './graphql/queries';
+import gql from 'graphql-tag';
+import { ListAuctionsQueryVariables, ListAuctionsQuery } from './API';
+import { AuctionCard } from './AuctionCard';
+
+export const Auctions = () => {
+  return (
+    <Query<ListAuctionsQuery, ListAuctionsQueryVariables> query={gql(listAuctions)} variables={{ limit: 4}}>
+      {({ data, loading }) => {
+        if (loading || !data || !data.listAuctions || !data.listAuctions.items) {
+          return null;
+        }
+
+        console.log(data.listAuctions.items)
+        
+        return (
+          <div style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(4, 1fr)',
+            gridGap: 10,
+          }}>
+            {data.listAuctions.items.map(x => <AuctionCard key={x!.id} name={x!.name} price={x!.price}  />)}
+          </div>
+        )
+      }}      
+    </Query>
+  );
+}
